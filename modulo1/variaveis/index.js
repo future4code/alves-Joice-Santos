9029d33dbd18096c64c2da0ec67632e78ba83525
Joice dos Santos <joice.pakua@gmail.com>
2022-05-09 21:07:56 -0300

    variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,22 @@
+// na primeira opção será impresso 10, na segunda opção quando muda o valor de b será impresso 10, 5
+// após a mudança de valores ficará impresso 10, 10, 10
+// p = horasDiariasTrabalhadas t= remuneraçãoDiaria
+let nome
+let idade
+typeof nome
+typeof idade
+console.log("nome")
+console.log("typeof nome")
+console.log("idade")
+console.log("typeof nome")
+// foi impressa essa resposta pois não atribui valores as variaveis
+nome = prompt("Qual seu nome?")
+idade = prompt("Qual sua idade?")
+console.log("Olá!", nome, "você tem", idade, "anos")
+// exercicio2
+let café = prompt("Você tomou café hoje?")
+let exercicio = prompt("Você se exercitou hoje?")
+let livro = prompt("você pretende ler um livro esse mês?")
+
+
+
