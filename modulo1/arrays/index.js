555ccb1faf2de237e218a1be3c80b16a4e452328
Joice dos Santos <joice.pakua@gmail.com>
2022-05-12 14:42:27 -0300

    adicionei-arrays

---
+++ modulo1/arrays/index.js
@@ -0,0 +1,27 @@
+/*let array
+console.log('a. ', array)
+//vai imprimir indefinido pois o array não foi listado
+array = null
+console.log('b. ', array)
+// como o array foi definido como null, vai imprimir null
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)
+// usando a propriedade length vai retornar a quantidade de itens no array, então vai imprimir 11
+let i = 0
+console.log('d. ', array[i])
+// vai retornar o item que está na posição 0 do array, então vai imprimir 3
+array[i+1] = 19 
+console.log('e. ', array)
+// irá substituir o número que está na posição 1 por 19
+const valor = array[i+6]
+console.log('f. ', valor)
+// vai retornar o valor de i somado a 6, então vai imprimir 9 */
+
+// 2- irá imprimir no console "SUBI NUM ÔNIBUS EM MIRROCOS"
+
+let nomeDoUsuario = prompt("Digite seu nome aqui")
+let emailDoUsuario = prompt("Digite seu e-mail aqui")
+console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem vinda(o) ${nomeDoUsuario}`)
+const arrayComidas = ["sushi", "churrasco", "pipoca", "Capelleti", "pizza"]
+console.log(arrayComidas)
+
