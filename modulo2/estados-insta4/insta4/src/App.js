9b6d772e7ee64ddca997ab12835a04749b4c6ce1
Joice dos Santos <joice.pakua@gmail.com>
2022-06-08 22:22:59 -0300

    adicionei-insta4

---
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+        <Post
+          nomeUsuario={'Luke'}
+          fotoUsuario={'https://picsum.photos/id/1/50/50'}
+          fotoPost={'https://picsum.photos/id/1025/200/150'}
+        />
+
+        <Post
+          nomeUsuario={'Leia'}
+          fotoUsuario={'https://picsum.photos/id/1027/50/50'}
+          fotoPost={'https://picsum.photos/id/1037/200/150'}
+        />
+
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
