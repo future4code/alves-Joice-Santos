50a21db0509952ca136400c1696542135a45f929
Joice dos Santos <joice.pakua@gmail.com>
2022-06-10 19:16:27 -0300

    adicionei-whatslab

---
+++ modulo2/projeto-whatslab/whatslab/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import './App.css';
+
+/*
+export default class Whatslab extends React.Component {
+  state = {
+    usuario: "",
+    msg: ""
+  };
+
+    adicionaUsuario = (event) => {
+      this.setState({ u: event.target.value });
+    };
+    adicionaMsg = (event) => {
+      const mensagem = event.target.value;
+      this.setState({ idade: idade });
+    };
+  }
+}
+*/
+
+return (
+  <div>
+    <h1> Whatslab</h1>
+  </div>
+)
