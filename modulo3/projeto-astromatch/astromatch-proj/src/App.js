178277678f03c4f5331250126ab534fb582de686
Joice dos Santos <joice.pakua@gmail.com>
2022-07-08 20:51:55 -0300

    projeto-dia-tres

--- modulo3/projeto-astromatch/astromatch-proj/src/App.js
+++ modulo3/projeto-astromatch/astromatch-proj/src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import header from "./components/header";
+import pagePerfil from "./components/pagePerfil";
+import pageMatches from "./components/pageMatches"
 
-function App() {
-  return (
-    <div>
-      Astromatch
-    </div>
+export default function App() {
+  const [valorPagina, setValorPagina] = useState(0);
+  const [novoEstado, setNovoEstado] = useState("AstroMatch");
+  const [pagina, setPagina] = useState("pagePerfil");
+
+  const trocaPagina = (paginaEscolhida) => {
+    setPagina(paginaEscolhida);
+  };
+  
+  return ( 
+  <div>
+    < header />
+    {novoEstado}
+    <button onClick={() => trocaPagina(pagePerfil)}>Ir para Perfil</button>
+    <button onClick={() => trocaPagina(pageMatches)}>Ir para Matches</button>
+  </div>
   );
 }
-
-export default App;
