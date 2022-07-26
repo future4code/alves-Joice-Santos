b7926bc2dda08a8078a11588f13d5ac4a9b42c4d
Joisantos <joice.pakua@gmail.com>
2022-07-25 23:22:44 -0300

    adicionei estrutura inicial

---
+++ modulo3/cookenu/cookenu/src/routes/Router.js
@@ -0,0 +1,26 @@
+import { Error } from "@material-ui/icons";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddRecipe from "../pages/AddRecipes/AddRecipes";
+import ErrorPage from "../pages/Error/ErrorPage";
+import Login from "../pages/Login/Login";
+import RecipeDetail from "../pages/RecipeDetail/RecipeDetail";
+import RecipeList from "../pages/RecipesList/RecipesList";
+import SignUp from "../pages/SignUp/SignUp";
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="login" element={ <Login/>} />
+                <Route path="cadastro" element={<SignUp/>} />   
+                <Route index element={<RecipeList/>} />
+                <Route path="adicionar-receita" element={<AddRecipe/>} />      
+                <Route path="detalhe/:id" element={<RecipeDetail/>} />
+                <Route path="*" element={<ErrorPage />} />     
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router
