38bf012379b0f294f15f5bed05411b4bcc1f39b8
Joisantos <joice.pakua@gmail.com>
2022-08-18 14:59:04 -0300

    exercicios

--- modulo4/fluxo-de-dados-backend/src/index.ts
+++ modulo4/fluxo-de-dados-backend/src/index.ts
@@ -1,6 +1,7 @@
-import express, { Request, Response } from "express"
+import express, { Request, response, Response } from "express"
 import cors from "cors"
 import { produtos, listaProdutos } from "./data"
+import { request } from "http"
 
 const app = express()
 
@@ -12,11 +13,32 @@ app.get("/test", (req: Request, res: Response) => {
     res.send("Até aqui tudo bem!")
 })
 
-//exercicio 2
-app.get("/produtos", (req: Request, res:Response) => {
-     
+//exercicio 3
+app.post("/cadastrarProduto", (req: Request, res:Response) => {
+    try {
+        const novoProduto: produtos = {
+            id: Date.now().toString(),
+            name: req.body.name,
+            price: req.body.price
+        }
+        listaProdutos.push(novoProduto)
+
+        response.send(listaProdutos)
+
+    } catch (error: any) {
+        response.status(response.statusCode || 500).send({message: error.message})
+    }
 })
 
+//exercicio 4
+app.get("/produtos", (req: Request, res:Response) => {
+    try {
+       res.send(listaProdutos)
+    } catch (error: any) {
+       res.status(res.statusCode || 500).send({message: error.message})
+    }
+    
+})
 
 app.listen(3003, () => {
     console.log("Servidor rodando na porta 3003")
