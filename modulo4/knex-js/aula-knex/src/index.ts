c168df9608645e5e7b6e1c25941a8c229bd2ad3f
Joisantos <joice.pakua@gmail.com>
2022-08-25 22:25:38 -0300

    exercicios knex

--- modulo4/knex-js/aula-knex/src/index.ts
+++ modulo4/knex-js/aula-knex/src/index.ts
@@ -5,26 +5,41 @@ import express from "express"
 import cors from "cors"
 
 
-const getActorById = async (id: string): Promise<any> => {
-  const result = await connection.raw(`
-    SELECT * FROM Actor WHERE id = '${id}'
-    `)
-
-    return result[0][0]
-}
-
 app.get("/users/:id", async (req: Request, res: Response) => {
   try {
     connection.raw(`
-    SELECT FROM Actor WHERE id = '${id}'
+    SELECT FROM Actor WHERE id = ()
     `)
-
-    return result[0][0]
-    `)
-
+                                
     res.end()
   } catch (error: any) {
 		console.log(error.message)
     res.status(500).send("Unexpected error")
   }
 }) 
+/*
+app.get("/users/:name", async (req: Request, res: Response) => {
+  try {
+    connection.raw(`
+    SELECT FROM Actor WHERE name = '${name}'
+    `)
+                                
+    res.end()
+  } catch (error: any) {
+		console.log(error.message)
+    res.status(500).send("Unexpected error")
+  }
+}) 
+
+app.get("/users/:gender", async (req: Request, res: Response) => {
+  try {
+    connection.raw(`
+    SELECT FROM Actor WHERE gender = '${gender}'
+    `)
+                                
+    res.end()
+  } catch (error: any) {
+		console.log(error.message)
+    res.status(500).send("Unexpected error")
+  }
+}) */
